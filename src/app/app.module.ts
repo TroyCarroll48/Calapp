9b100670d5c95cf0da3e45c41e26e571e8fb1420
TroyCarroll48 <troycarroll48@yahoo.com>
2018-11-28 12:42:14 -0500

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CalComponent } from './cal/cal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalComponent
   ],
   imports: [
     BrowserModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/cal/cal.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cal',
+  templateUrl: './cal.component.html',
+  styleUrls: ['./cal.component.css']
+})
+export class CalComponent implements OnInit {
+
+  public num1 :number;
+  public num2 :number;
+  public result :number;
+
+  cal_add()
+  {
+
+    this.result = this.num1+this.num2;
+  }
+  constructor(){
+
+  }
+
+  ngOnInit() {
+  }
+
+}
